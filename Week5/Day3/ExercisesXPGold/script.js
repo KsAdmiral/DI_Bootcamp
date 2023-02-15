1aa065103b48f4c38c0a58c552f7addc5d5f3995
KsAdmiral <mkilnivz@gmail.com>
2023-02-15 22:55:06 +0200

    ExercisesXPGold done

---
+++ Week5/Day3/ExercisesXPGold/script.js
@@ -0,0 +1,24 @@
+// Exercise 1 : Star Wars API
+// Instructions
+// Part I
+// Using this code:
+
+const urls = [
+  "https://www.swapi.tech/api/people/1",
+  "https://www.swapi.tech/api/people/2",
+  "https://www.swapi.tech/api/people/3",
+  "https://www.swapi.tech/api/people/4",
+];
+
+// Use Promise.all to fetch all the characters from the array above with only one request.
+// Console.log the output and make sure it has a catch block as well.
+let requests = urls.map(url => fetch(url));
+
+Promise.all(requests).then(responses =>
+  Promise.all(responses.map(r => r.json()))
+    .then(users => users.forEach(user => console.log(user.result.properties)))
+    .catch(error => console.eroor(error))
+);
+
+// Part II
+// Change one of the urls in the array provided above. This should cause an error. Make sure your catch block works.
